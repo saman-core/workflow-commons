5e27ef73f9074b3e089ed48b8bdec162688ca14b
Hernael <hperer@movealong.tech>
2025-05-25 20:57:53 -0400

    fix StateCategoryType

--- model/src/main/java/io/samancore/workflow/model/StateCategoryType.java
+++ model/src/main/java/io/samancore/workflow/model/StateCategoryType.java
@@ -1,9 +1,22 @@
 package io.samancore.workflow.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum StateCategoryType {
-    INITIAL,
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED,
-    EXCLUDED
+    INITIAL("INITIAL"),
+    PENDING("PENDING"),
+    IN_PROGRESS("IN_PROGRESS"),
+    COMPLETED("COMPLETED"),
+    EXCLUDED("EXCLUDED"),;
+
+    private final String value;
+
+    StateCategoryType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
